8114f44e77f8e2b917719eacd9fba81c6201fc93
Min Koo <62103197+koomin1227@users.noreply.github.com>
2023-05-16 23:01:01 +0900

    Update PSNR.c

--- week4/PSNR.c
+++ week4/PSNR.c
@@ -45,40 +45,6 @@ int main() {
 		{
 			Y1[j * width + i] = inputImg[j * stride + 3 * i + 0];
 			Y2[j * width + i] = inputImg1[j * stride + 3 * i + 0];
-			//Y2 = 0.299  * inputImg1[j * stride + 3 * i + 2] + 0.587 * inputImg1[j * stride + 3 * i + 1] + 0.114 * inputImg1[j * stride + 3 * i + 0];
-			//Y2 = (unsigned char)(Y2 > 255 ? 255 : (Y2 < 0 ? 0 : Y2));
-			//mse += (double)((Y2 - Y1)*(Y2 - Y1));
-
-
-
-
-
-
-
-
-
-			//Y = 0.299  * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
-			//Cb = -0.169 * inputImg[j * stride + 3 * i + 2] - 0.331 * inputImg[j * stride + 3 * i + 1] + 0.500 * inputImg[j * stride + 3 * i + 0];
-			//Cr = 0.500 * inputImg[j * stride + 3 * i + 2] - 0.419 * inputImg[j * stride + 3 * i + 1] - 0.0813 * inputImg[j * stride + 3 * i + 0];
-			//I = (unsigned char)((inputImg[j * stride + 3 * i + 2] + inputImg[j * stride + 3 * i + 1] + inputImg[j * stride + 3 * i + 0]) / 3);
-			////outputImg[j * stride + 3 * i + 0] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
-			////outputImg[j * stride + 3 * i + 1] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
-			////outputImg[j * stride + 3 * i + 2] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
-			////outputImg[j * stride + 3 * i + 0] = I;
-			////outputImg[j * stride + 3 * i + 1] = I;
-			////outputImg[j * stride + 3 * i + 2] = I;
-			//diff_Y= (Y - I)*(Y - I);
-			//outputImg[j * stride + 3 * i + 0] = (unsigned char)(diff_Y > 255 ? 255 : (diff_Y < 0 ? 0 : diff_Y));
-			//outputImg[j * stride + 3 * i + 1] = (unsigned char)(diff_Y > 255 ? 255 : (diff_Y < 0 ? 0 : diff_Y));
-			//outputImg[j * stride + 3 * i + 2] = (unsigned char)(diff_Y > 255 ? 255 : (diff_Y < 0 ? 0 : diff_Y));
-			///*Y = Y + 20;
-			//R = (Y + 100) + 1.402 * Cr;
-			//G = (Y + 100) - 0.714 * Cr - 0.344 *Cb;
-			//B = (Y + 100) + 1.772 * Cb;
-			//outputImg[j * stride + 3 * i + 2] = (unsigned char)(R > 255 ? 255 : (R < 0 ? 0 : R));
-			//outputImg[j * stride + 3 * i + 1] = (unsigned char)(G > 255 ? 255 : (G < 0 ? 0 : G));
-			//outputImg[j * stride + 3 * i + 0] = (unsigned char)(B > 255 ? 255 : (B < 0 ? 0 : B));*/
-			////printf("%lf", Y);
 		}
 	}
 	for (int j = 0; j < height; j++)
