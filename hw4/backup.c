b09842e6e5f0a50b6982469423d82f7d66ae2a62
Min Koo <62103197+koomin1227@users.noreply.github.com>
2023-05-17 15:04:37 +0900

    Update backup.c

--- hw4/backup.c
+++ hw4/backup.c
@@ -8,7 +8,7 @@ void make_Ypicture()
 {
 	BITMAPFILEHEADER bmpFile;
 	BITMAPINFOHEADER bmpInfo;
-	FILE *inputFile = NULL;
+	FILE* inputFile = NULL;
 	inputFile = fopen("front.bmp", "rb");
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile);
 	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFile);
@@ -21,9 +21,9 @@ void make_Ypicture()
 	double Y, Cb, Cr;
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width, stride, height, size, bitCnt);
 
-	unsigned char *inputImg = NULL, *outputImg = NULL;
-	inputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
-	outputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
+	unsigned char* inputImg = NULL, *outputImg = NULL;
+	inputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	outputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
 	fread(inputImg, sizeof(unsigned char), size, inputFile);
 
 	//Original Copy
@@ -31,14 +31,14 @@ void make_Ypicture()
 	{
 		for (int i = 0; i < width; i++)
 		{
-			Y = 0.299  * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
+			Y = 0.299 * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
 			outputImg[j * stride + 3 * i + 0] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
 			outputImg[j * stride + 3 * i + 1] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
 			outputImg[j * stride + 3 * i + 2] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
 		}
 	}
 
-	FILE *outputFile = fopen("frontY.bmp", "wb");
+	FILE * outputFile = fopen("frontY.bmp", "wb");
 	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile);
 	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile);
 	fwrite(outputImg, sizeof(unsigned char), size, outputFile);
@@ -55,7 +55,7 @@ void downSampling()
 {
 	BITMAPFILEHEADER bmpFile, bmpFile1;
 	BITMAPINFOHEADER bmpInfo, bmpInfo1;
-	FILE *inputFile1 = NULL;
+	FILE* inputFile1 = NULL;
 	inputFile1 = fopen("frontY.bmp", "rb");
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile1);
 	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFile1);
@@ -66,16 +66,16 @@ void downSampling()
 	int size = bmpInfo.biSizeImage;
 	int bitCnt = bmpInfo.biBitCount;
 	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
-	unsigned char *Y1, *Y2;
+	unsigned char* Y1, *Y2;
 
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width, stride, height, size, bitCnt);
 
-	unsigned char *inputImg = NULL, *outputImg_down = NULL;
-	inputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
+	unsigned char* inputImg = NULL, *outputImg_down = NULL;
+	inputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
 
 
 	fread(inputImg, sizeof(unsigned char), size, inputFile1);
-	Y1 = (unsigned char *)malloc(sizeof(unsigned char)*(width * height));
+	Y1 = (unsigned char*)malloc(sizeof(unsigned char) * (width * height));
 
 
 	for (int j = 0; j < height; j++)
@@ -91,9 +91,9 @@ void downSampling()
 	int stride2 = (((bitCnt / 8) * width2) + 3) / 4 * 4;
 	//int size2 = stride2 * height2;
 	int size2 = stride2 * height2;
-	Y2 = (unsigned char *)malloc(sizeof(unsigned char)*(width2 * height2));
+	Y2 = (unsigned char*)malloc(sizeof(unsigned char) * (width2 * height2));
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width2, stride2, height2, size2, bitCnt);
-	outputImg_down = (unsigned char *)calloc(size2, sizeof(unsigned char));
+	outputImg_down = (unsigned char*)calloc(size2, sizeof(unsigned char));
 	for (int j = 0; j < height2; j++)
 	{
 		for (int i = 0; i < width2; i++)
@@ -116,7 +116,7 @@ void downSampling()
 	}
 
 
-	FILE *outputFile_down = fopen("output_down.bmp", "wb");
+	FILE* outputFile_down = fopen("output_down.bmp", "wb");
 
 	bmpInfo.biWidth = width2;
 	bmpInfo.biHeight = height2;
@@ -146,7 +146,7 @@ void upsampling()
 {
 	BITMAPFILEHEADER bmpFile, bmpFile1;
 	BITMAPINFOHEADER bmpInfo, bmpInfo1;
-	FILE *inputFile1 = NULL;
+	FILE* inputFile1 = NULL;
 	inputFile1 = fopen("output_down.bmp", "rb");
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile1);
 	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFile1);
@@ -157,16 +157,16 @@ void upsampling()
 	int size = bmpInfo.biSizeImage;
 	int bitCnt = bmpInfo.biBitCount;
 	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
-	unsigned char *Y1, *Y2;
+	unsigned char* Y1, *Y2;
 
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width, stride, height, size, bitCnt);
 
-	unsigned char *inputImg = NULL, *outputImg_down = NULL, *outputImg_up = NULL;
-	inputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
+	unsigned char* inputImg = NULL, *outputImg_down = NULL, *outputImg_up = NULL;
+	inputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
 
 
 	fread(inputImg, sizeof(unsigned char), size, inputFile1);
-	Y1 = (unsigned char *)malloc(sizeof(unsigned char)*(width * height));
+	Y1 = (unsigned char*)malloc(sizeof(unsigned char) * (width * height));
 
 
 	for (int j = 0; j < height; j++)
@@ -182,10 +182,12 @@ void upsampling()
 	int stride2 = (((bitCnt / 8) * width2) + 3) / 4 * 4;
 	int size2 = stride2 * height2;
 	int nj, ni;
-	Y2 = (unsigned char *)malloc(sizeof(unsigned char)*(width2 * height2));
+	Y2 = (unsigned char*)malloc(sizeof(unsigned char) * (width2 * height2));
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width2, stride2, height2, size2, bitCnt);
-	outputImg_down = (unsigned char *)calloc(size2, sizeof(unsigned char));
-	outputImg_up = (unsigned char *)calloc(size2, sizeof(unsigned char));
+	outputImg_down = (unsigned char*)calloc(size2, sizeof(unsigned char));
+	outputImg_up = (unsigned char*)calloc(size2, sizeof(unsigned char));
+	double inter_filter[5][2] = { {1,0},{3.0 / 4,1.0 / 4},{2.0 / 4, 2.0 / 4},{1.0 / 4,3.0 / 4}, {0, 1} };
+	double u, d, l, r;
 	for (int j = 0; j < height; j++)
 	{
 		for (int i = 0; i < width; i++)
@@ -194,29 +196,56 @@ void upsampling()
 				printf("d");
 			if (j < height - 1 && i < width - 1)
 			{
-				Y2[(j * 4) * width2 + (i * 4)] = Y1[(j) * width + (i)];
-				Y2[(j * 4) * width2 + (i * 4 + 1)] = (Y1[(j)* width + (i)] + Y1[(j)* width + (i + 1)])/2;
-				Y2[(j * 4) * width2 + (i * 4 + 2)] = Y1[(j)* width + (i + 1)];
+				u = Y1[(j)* width + (i)] * inter_filter[2][0] + Y1[(j)* width + (i + 1)] * inter_filter[2][1];
+				d = Y1[(j + 1) * width + (i)] * inter_filter[2][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[2][1];
+				l = Y1[(j)* width + (i)] * inter_filter[2][0] + Y1[(j + 1) * width + (i)] * inter_filter[2][1];
+				r = Y1[(j)* width + (i + 1)] * inter_filter[2][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[2][1];
 
-				Y2[(j * 4+1) * width2 + (i * 4)] = (Y1[(j)* width + (i)] + Y1[(j + 1)* width + (i)])/2;
-				Y2[(j * 4+1) * width2 + (i * 4 + 1)] = (Y1[(j)* width + (i)] + Y1[(j)* width + (i + 1)] + Y1[(j + 1)* width + (i)] + Y1[(j + 1)* width + (i + 1)])/4;
-				Y2[(j * 4+1) * width2 + (i * 4 + 2)] = (Y1[(j)* width + (i + 1)] + Y1[(j + 1)* width + (i + 1)]) / 2;
+				Y2[(j * 4) * width2 + (i * 4)] = Y1[(j)* width + (i)];
+				Y2[(j * 4) * width2 + (i * 4 + 1)] = Y1[(j)* width + (i)] * inter_filter[1][0] + Y1[(j)* width + (i + 1)] * inter_filter[1][1];
+				Y2[(j * 4) * width2 + (i * 4 + 2)] = Y1[(j)* width + (i)] * inter_filter[2][0] + Y1[(j)* width + (i + 1)] * inter_filter[2][1];
+				Y2[(j * 4) * width2 + (i * 4 + 3)] = Y1[(j)* width + (i)] * inter_filter[3][0] + Y1[(j)* width + (i + 1)] * inter_filter[3][1];
+				Y2[(j * 4) * width2 + (i * 4 + 4)] = Y1[(j)* width + (i + 1)];
 
-				Y2[(j * 4+2) * width2 + (i * 4)] = Y1[(j + 1)* width + (i)];
-				Y2[(j * 4+2) * width2 + (i * 4 + 1)] = (Y1[(j + 1)* width + (i)] + Y1[(j + 1)* width + (i + 1)])/2;
-				Y2[(j * 4+2) * width2 + (i * 4 + 2)] = Y1[(j + 1)* width + (i + 1)];
+				Y2[(j * 4 + 1) * width2 + (i * 4)] = Y1[(j) * width + (i)] * inter_filter[1][0] + Y1[(j + 1) * width + (i)] * inter_filter[1][1];
+				Y2[(j * 4 + 1) * width2 + (i * 4 + 1)] = Y1[(j)* width + (i)] * inter_filter[1][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[1][1];
+				Y2[(j * 4 + 1) * width2 + (i * 4 + 2)] = u * inter_filter[1][0] + d * inter_filter[1][1];
+				Y2[(j * 4 + 1) * width2 + (i * 4 + 3)] = Y1[(j)* width + (i + 1)] * inter_filter[1][0] + Y1[(j + 1) * width + (i)] * inter_filter[1][1];
+				Y2[(j * 4 + 1) * width2 + (i * 4 + 4)] = Y1[(j)* width + (i + 1)] * inter_filter[1][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[1][1];
+								
+				Y2[(j * 4 + 2) * width2 + (i * 4)] = Y1[(j)* width + (i)] * inter_filter[2][0] + Y1[(j + 1) * width + (i)] * inter_filter[2][1];
+				Y2[(j * 4 + 2) * width2 + (i * 4 + 1)] = l * inter_filter[1][0] + r * inter_filter[1][1];
+				Y2[(j * 4 + 2) * width2 + (i * 4 + 2)] = (Y1[(j)* width + (i)] + Y1[(j)* width + (i + 1)] + Y1[(j + 1) * width + (i)] + Y1[(j + 1) * width + (i + 1)]) / 4;
+				Y2[(j * 4 + 2) * width2 + (i * 4 + 3)] = l * inter_filter[3][0] + r * inter_filter[3][1];
+				Y2[(j * 4 + 2) * width2 + (i * 4 + 4)] = Y1[(j)* width + (i + 1)] * inter_filter[2][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[2][1];
+			
+				Y2[(j * 4 + 3) * width2 + (i * 4)] = Y1[(j)* width + (i)] * inter_filter[3][0] + Y1[(j + 1) * width + (i)] * inter_filter[3][1];
+				Y2[(j * 4 + 3) * width2 + (i * 4 + 1)] = Y1[(j)* width + (i + 1)] * inter_filter[3][0] + Y1[(j + 1) * width + (i)] * inter_filter[3][1];
+				Y2[(j * 4 + 3) * width2 + (i * 4 + 2)] = u * inter_filter[3][0] + d * inter_filter[3][1];
+				Y2[(j * 4 + 3) * width2 + (i * 4 + 3)] = Y1[(j)* width + (i)] * inter_filter[3][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[3][1];
+				Y2[(j * 4 + 3) * width2 + (i * 4 + 4)] = Y1[(j)* width + (i + 1)] * inter_filter[3][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[3][1];
+			
+				Y2[(j * 4 + 4) * width2 + (i * 4)] = Y1[(j + 1)* width + (i)];
+				Y2[(j * 4 + 4) * width2 + (i * 4 + 1)] = Y1[(j + 1)* width + (i)] * inter_filter[1][0] + Y1[(j + 1)* width + (i + 1)] * inter_filter[1][1];
+				Y2[(j * 4 + 4) * width2 + (i * 4 + 2)] = Y1[(j + 1) * width + (i)] * inter_filter[2][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[2][1];
+				Y2[(j * 4 + 4) * width2 + (i * 4 + 3)] = Y1[(j + 1) * width + (i)] * inter_filter[3][0] + Y1[(j + 1) * width + (i + 1)] * inter_filter[3][1];
+				Y2[(j * 4 + 4) * width2 + (i * 4 + 4)] = Y1[(j + 1)* width + (i)];
 			}
 			else if (j == height - 1 && i < width - 1)
 			{
 				Y2[(j * 4) * width2 + (i * 4)] = Y1[(j)* width + (i)];
 				Y2[(j * 4) * width2 + (i * 4 + 1)] = (Y1[(j)* width + (i)] + Y1[(j)* width + (i + 1)]) / 2;
 				Y2[(j * 4) * width2 + (i * 4 + 2)] = Y1[(j)* width + (i + 1)];
+				Y2[(j * 4) * width2 + (i * 4 + 3)] = Y1[(j)* width + (i + 1)];
+				Y2[(j * 4) * width2 + (i * 4 + 4)] = Y1[(j)* width + (i + 1)];
 			}
 			else if (i == width - 1 && j < height - 1)
 			{
 				Y2[(j * 4) * width2 + (i * 4)] = Y1[(j)* width + (i)];
-				Y2[(j * 4 + 1) * width2 + (i * 4)] = (Y1[(j)* width + (i)] + Y1[(j + 1)* width + (i)]) / 2;
-				Y2[(j * 4 + 2) * width2 + (i * 4)] = Y1[(j + 1)* width + (i)];
+				Y2[(j * 4 + 1) * width2 + (i * 4)] = (Y1[(j)* width + (i)] + Y1[(j + 1) * width + (i)]) / 2;
+				Y2[(j * 4 + 2) * width2 + (i * 4)] = Y1[(j + 1) * width + (i)];
+				Y2[(j * 4 + 3) * width2 + (i * 4)] = Y1[(j + 1) * width + (i)];
+				Y2[(j * 4 + 4) * width2 + (i * 4)] = Y1[(j + 1) * width + (i)];
 			}
 			else if (j == height - 1 && i == width - 1)
 			{
@@ -238,7 +267,7 @@ void upsampling()
 	}
 
 
-	FILE *outputFile_down = fopen("output_up.bmp", "wb");
+	FILE* outputFile_down = fopen("output_up.bmp", "wb");
 
 	bmpInfo.biWidth = width2;
 	bmpInfo.biHeight = height2;
@@ -262,7 +291,7 @@ void psnr()
 {
 	BITMAPFILEHEADER bmpFile, bmpFile1;
 	BITMAPINFOHEADER bmpInfo, bmpInfo1;
-	FILE *inputFile1 = NULL, *inputFile2 = NULL;
+	FILE* inputFile1 = NULL, * inputFile2 = NULL;
 	inputFile1 = fopen("frontY.bmp", "rb");
 	inputFile2 = fopen("output_up.bmp", "rb");
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile1);
@@ -275,23 +304,23 @@ void psnr()
 	int size = bmpInfo.biSizeImage;
 	int bitCnt = bmpInfo.biBitCount;
 	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
-	double *Y1, Cb, Cr, R, G, B, *Y2;
+	double* Y1, Cb, Cr, R, G, B, *Y2;
 	double diff_Y;
 
 	unsigned char I;
 	printf("W: %d(%d)\nH: %d\nS: %d\nD: %d\n\n", width, stride, height, size, bitCnt);
 
-	unsigned char *inputImg = NULL, *outputImg = NULL, *inputImg1 = NULL;
-	inputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
-	inputImg1 = (unsigned char *)calloc(size, sizeof(unsigned char));
-	outputImg = (unsigned char *)calloc(size, sizeof(unsigned char));
+	unsigned char* inputImg = NULL, *outputImg = NULL, *inputImg1 = NULL;
+	inputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	inputImg1 = (unsigned char*)calloc(size, sizeof(unsigned char));
+	outputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
 	fread(inputImg, sizeof(unsigned char), size, inputFile1);
 	fread(inputImg1, sizeof(unsigned char), size, inputFile2);
 
 	//Original Copy
 	double mse = 0, psnr;
-	Y1 = (double *)calloc(sizeof(double), (width * height));
-	Y2 = (double *)calloc(sizeof(double), (width * height));
+	Y1 = (double*)calloc(sizeof(double), (width * height));
+	Y2 = (double*)calloc(sizeof(double), (width * height));
 	for (int j = 0; j < height; j++)
 	{
 		for (int i = 0; i < width; i++)
@@ -304,11 +333,11 @@ void psnr()
 	{
 		for (int i = 0; i < width; i++)
 		{
-			mse += (double)((Y2[j*width + i] - Y1[j*width + i])*(Y2[j*width + i] - Y1[j*width + i]));
+			mse += (double)((Y2[j * width + i] - Y1[j * width + i]) * (Y2[j * width + i] - Y1[j * width + i]));
 		}
 	}
-	mse /= (width *height);
-	psnr = mse != 0.0 ? 10.0* log10((255 * 255) / mse) : 99.99;
+	mse /= (width * height);
+	psnr = mse != 0.0 ? 10.0 * log10((255 * 255) / mse) : 99.99;
 	printf("MSE = %.2lf\nPSNR = %.2lf db\n", mse, psnr);
 
 	free(outputImg);
@@ -319,8 +348,8 @@ void psnr()
 
 int main() {
 	//make_Ypicture();
-	//downSampling();
+	downSampling();
 	upsampling();
-	//psnr();
+	psnr();
 	return 0;
 }
