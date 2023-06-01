cdacf49de5e8c1cc098a634b3222efe4f0e47c49
Min Koo <62103197+koomin1227@users.noreply.github.com>
2023-06-01 16:06:35 +0900

    Update bin_decoder.c

--- week13/bin_decoder.c
+++ week13/bin_decoder.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <math.h>
@@ -34,22 +35,34 @@ int main() {
 	Y1 = (unsigned char*)malloc(sizeof(unsigned char) * (width * height));
 	R = (unsigned char*)malloc(sizeof(unsigned char) * (width * height));
 
-	char tmp[3] = { 0 };
+	char tmp[4] = { 0 };
+	int error = 0;
 
 	for (int j = 0; j < height; j++)
 	{
 		for (int i = 0; i < width; i++)
 		{
 			//fscanf(textFile, "%d ", &tmp);
-			fread(tmp, 3,1, textFile);
-			Y1[j * width + i] = (unsigned char)tmp;
-			Y1[j * width + i] = Y1[j * width + i] * 5;
+			fread(tmp, 3, 1, textFile);
+
+			if (strcmp(tmp, "000") == 0) error = -4;
+			else if (strcmp(tmp, "001") == 0) error = -3;
+			else if (strcmp(tmp, "010") == 0) error = -2;
+			else if (strcmp(tmp, "011") == 0) error = -1;
+			else if (strcmp(tmp, "100") == 0) error = 0;
+			else if (strcmp(tmp, "101") == 0) error = 1;
+			else if (strcmp(tmp, "110") == 0) error = 2;
+			else if (strcmp(tmp, "111") == 0) error = 3;
+
+			//Y1[j * width + i] = (unsigned char)error;
+			//Y1[j * width + i] = Y1[j * width + i] * 5;
+			error = error * 5;
 			if (i == 0)
 			{
-				R[j * width + i] = 128 + Y1[j * width + i];
+				R[j * width + i] = 128 + error;
 			}
 			else {
-				R[j * width + i] = R[j * width + i - 1] + Y1[j * width + i];
+				R[j * width + i] = R[j * width + i - 1] + error;
 			}
 		}
 	}
@@ -76,5 +89,6 @@ int main() {
 
 	fclose(inputFile1);
 	fclose(textFile);
+	fclose(outputFile_rotate);
 	return 0;
 }
